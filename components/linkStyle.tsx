a98dcf208ad41cacc1c4fabcd7eec2c48814a7dc
jerncomania28 <okonnduesojeremiah@gmail.com>
2022-12-24 09:54:49 +0000

    feat:animation for mobile navigation complete!

--- components/linkStyle.tsx
+++ components/linkStyle.tsx
@@ -9,7 +9,9 @@ interface LinkStyleProps {
 
 const LinkStyle = ({ children, href }: LinkStyleProps) => {
     return (
-        <div style={{ "fontFamily": "logo" }} className="relative inline-block text-custom-grey capitalize mx-6 before:transition-all before:duration-300 before:ease-in-out before:content-[''] before:h-[1px] before:absolute before:-bottom-1 before:left-0 before:w-[40%] before:bg-charcoal hover:before:w-full ">
+        <div
+            style={{ "fontFamily": "logo" }}
+            className={`relative inline-block text-custom-grey capitalize mx-6 before:transition-all before:duration-300 before:ease-in-out before:content-[''] before:h-[1px] before:absolute before:-bottom-1 before:left-0 before:w-[40%] before:bg-charcoal hover:before:w-full`}>
             <Link href={href} >{children} </Link>
         </div>
     )
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,9 +1,25 @@
 import Link from "next/link";
 
-import { useState } from "react";
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 //components
 import LinkStyle from "./linkStyle";
+
+
+const MobileLinkStyle = ({ children, index }: { children: React.ReactNode, index: number }) => {
+    return (
+        <motion.div
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1, delay: (index * 0.2) }}
+            exit={{ y: -50, opacity: 0 }}
+            className={`relative text-[30px] inline-block text-custom-grey capitalize my-4 before:transition-all before:duration-300 before:ease-in-out before:content-[''] before:h-[1px] before:absolute before:-bottom-1 before:left-0 before:w-[40%] before:bg-charcoal hover:before:w-full`}>
+            <Link href={`/${children}`}>{children}</Link>
+        </motion.div>
+    )
+
+}
 
 
 const Navigation = () => {
@@ -38,16 +54,38 @@ const Navigation = () => {
                 </div>
 
 
-                <div className="w-6 h-6 relative text-center transition-all duration-500 ease-in-out md:hidden" onClick={handleSetIsOpen}>
-
+                <div className="w-6 h-6 relative text-center transition-all duration-500 ease-in-out md:hidden z-20" onClick={handleSetIsOpen}>
                     {/* menu Btn */}
                     <span className={`w-full h-[3px] transparent rounded absolute top-[50%] left-0 transition-all duration-500 ease-in-out before:content-[''] before:h-[3px] before:bg-charcoal before:rounded before:absolute before:-top-[5px] before:w-full before:left-0 before:transition-all before:duration-500 before:ease-in-out after:content-[''] after:h-[3px] after:bg-charcoal after:rounded after:absolute after:top-[5px] after:w-[60%] after:left-0 after:transition-all after:duration-500 after:ease-in-out ${isOpen && "before:transform before:translate-y-[5px] before:rotate-45 after:-translate-y-[5px] after:-rotate-45 after:w-full"}`}>
                     </span>
-
-
                 </div>
 
             </div>
+
+            {/* animate navigation menu on mobile on entr and exit */}
+
+            <AnimatePresence>
+                {
+                    isOpen && (
+                        <motion.div
+                            initial={{ y: -200, opacity: 0 }}
+                            animate={{ y: 0, opacity: 1 }}
+                            transition={{ duration: 0.3, delay: 0.5 }}
+                            exit={{ y: -200, opacity: 0 }}
+                            style={{ "fontFamily": "logo" }}
+                            className={`w-full h-[100vh] fixed top-0 left-0 bg-black z-10 flex items-center justify-center flex-col md:hidden `}
+                        >
+                            {
+                                NAV_ROUTES.map((route, _idx) => (
+                                    <MobileLinkStyle key={_idx} index={_idx}>
+                                        {route}
+                                    </MobileLinkStyle>
+                                ))
+                            }
+                        </motion.div>
+                    )
+                }
+            </AnimatePresence>
 
         </nav>
     )
